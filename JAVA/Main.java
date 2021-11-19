37b12f26a75a9e91a4009b3d65e5e35eb6786ed7
ShinWonjung <dnjswjd1204@gmail.com>
2021-11-19 23:22:57 +0900

    10869

--- JAVA/Main.java
+++ JAVA/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("강한친구 대한육군");
-        System.out.println("강한친구 대한육군");
+
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+        
+        in.close();
     }
 }
