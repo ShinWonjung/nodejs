e84e488d79168d1dceaabc864c5df0364634e6a5
ShinWonjung <dnjswjd1204@gmail.com>
2021-11-19 23:27:39 +0900

    10430

--- JAVA/Main.java
+++ JAVA/Main.java
@@ -6,12 +6,12 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int A = in.nextInt();
         int B = in.nextInt();
+        int C = in.nextInt();
 
-        System.out.println(A + B);
-        System.out.println(A - B);
-        System.out.println(A * B);
-        System.out.println(A / B);
-        System.out.println(A % B);
+        System.out.println((A + B) % C);
+        System.out.println(((A % C) + (B % C)) % C);
+        System.out.println((A * B) % C);
+        System.out.println(((A % C) * (B % C)) % C);
         
         in.close();
     }
